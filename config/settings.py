09acf27093d7e33cac413a4e6714a4bab2008367
sanya-mahajan <88281057+sanya-mahajan@users.noreply.github.com>
2023-04-15 18:57:40 +0530

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['s-m.ccstiet.com']
 
 
 # Application definition
